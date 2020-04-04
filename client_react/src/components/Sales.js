7f16d8914d5b3dff49fd5ab1cacdf066804796e8
KJnong <57115252+KJnong@users.noreply.github.com>
2020-04-05 00:34:34 +0200

    calling salee api to get salees

--- client_react/src/components/Sales.js
+++ client_react/src/components/Sales.js
@@ -1,49 +1,44 @@
 import React from 'react';
 
-class Sales extends React.Component
-{
-    constructor() 
-    {
-        super();
+class Sales extends React.Component {
+    state =
+        {
+            salees: []
+        }
 
-        this.state = 
-      {
-          name : 'aa',
-          content : 'dd',
-          time : 'aa'
-      }
-        
+    loadContent = async () => {
+        const rawSales = await fetch('http://localhost:5000/salee')
+        const sales = await rawSales.json();
+
+        this.setState({ saless: sales });
+
+        console.log(this.state);
+
     }
 
-    loadContent = async () =>
-    {
-      const rawSales = await fetch('http://localhost:5000/salee')
-      const sales = await rawSales.json();
-
-      this.setState( 
-      {
-          name : sales.name,
-          content : sales.content,
-          time : sales.created
-      })
-
-      console.log(this.state);
-      
-    }
-
-    componentDidMount(){
+    componentDidMount() {
         this.loadContent();
     }
-    render()
-    {
-        return(
-            <div>
-              <small>{this.state.time}</small>
-              <small>{this.state.name}</small>
-              <p>
-                  <h5>{this.state.content}</h5>
-              </p>
-            </div>)
+    render() {
+
+        const { saless } = this.state;
+
+        if (!!saless) {
+
+            return (
+                <div>
+                    {saless.map(sale => (
+                        <div>
+                            <small>{sale.time}</small>
+                            <small>{sale.name}</small>
+                            <p>
+                                <h5>{sale.content}</h5>
+                            </p></div>))}
+                </div>)
+        }
+        else {
+            return <p>Ther are no Saless</p>
+        }
     }
 
 }
