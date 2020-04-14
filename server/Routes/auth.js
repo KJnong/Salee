95b871b138af18eaf575e457f61892f26caec004
KJnong <57115252+KJnong@users.noreply.github.com>
2020-04-14 23:41:33 +0200

    installed bcryptjs and installed register password

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userModel = require('../Models/User')
 const {RegiserValidation} = require("../Validation/Validation")
+const bcrypt = require('bcryptjs')
 
 router.post('/register', async (req , res)=>
 {    
@@ -16,12 +17,17 @@ router.post('/register', async (req , res)=>
         return res.status(401).send("Email already exist")
     }
 
+    //hash password
+    const salt = await bcrypt.genSalt()
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+
     const user = new userModel(
         {
             name : req.body.name,
             lastName : req.body.lastName,
             email : req.body.email,
-            password : req.body.password
+            password : hashedPassword
         })
 
     try
